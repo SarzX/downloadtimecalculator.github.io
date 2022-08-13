eeedb4f8024fe5185440c89d0dcafb2d4aa92109
SarzX <86235485+SarzX@users.noreply.github.com>
2022-08-13 14:52:23 +0500

    Add files via upload

---
+++ static/about.js
@@ -0,0 +1,6 @@
+//this function is for dark mode
+function darkmode() {
+    var element = document.body;
+    element.classList.toggle("dark");
+    document.getElementById("submit").style.backgroundColor = "rgb(130, 71, 224)";
+}
